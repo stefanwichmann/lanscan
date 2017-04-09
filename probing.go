8a5385ddbbab31e7832409bef30ad27592515663
Stefan Wichmann <mail@stefanwichmann.com>
2017-04-09 15:37:55 +0200

    remove ineffective assign

--- probing.go
+++ probing.go
@@ -30,7 +30,6 @@ const defaultTimeout = 50 * time.Millisecond
 func ProbeHosts(hosts <-chan string, port int, protocol string, respondingHosts chan<- string, done chan<- bool) {
 	adjustedTimeout := defaultTimeout
 	for host := range hosts {
-		adjustedTimeout := adjustedTimeout
 		start := time.Now()
 		con, err := net.DialTimeout(protocol, fmt.Sprintf("%s:%d", host, port), adjustedTimeout)
 		duration := time.Now().Sub(start)
